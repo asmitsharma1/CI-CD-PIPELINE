f75aeee32a1f6d625574b38b1da641128f79ba46
Asmit Sharma <asmitsharma895@gmail.com>
2026-01-13 09:44:16 +0530

    Added UI and status

--- server.js
+++ server.js
@@ -1,8 +1,54 @@
 const express = require("express");
 const app = express();
 
+const version = "v2.0";
+const deployedAt = new Date().toLocaleString();
+
 app.get("/", (req, res) => {
-  res.send("CI/ CD Pipeline Working Successfully 🚀yayaya");
+  res.send(`
+  <html>
+    <head>
+      <title>CI/CD Dashboard</title>
+      <style>
+        body{
+          font-family: Arial;
+          background:#0f172a;
+          color:white;
+          text-align:center;
+          padding:50px;
+        }
+        .card{
+          background:#1e293b;
+          padding:30px;
+          border-radius:12px;
+          width:400px;
+          margin:auto;
+        }
+        .success{
+          color:#22c55e;
+        }
+      </style>
+    </head>
+
+    <body>
+      <div class="card">
+        <h1 class="success">Deployment Successful 🚀</h1>
+        <p><b>Version:</b> ${version}</p>
+        <p><b>Last Deployed:</b> ${deployedAt}</p>
+        <p>Powered by Jenkins + Docker</p>
+      </div>
+    </body>
+  </html>
+  `);
+});
+
+app.get("/status", (req, res) => {
+  res.json({
+    status: "LIVE",
+    version: version,
+    deployedAt: deployedAt,
+    uptime: process.uptime(),
+  });
 });
 
 app.listen(3000, () => {
